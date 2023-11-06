37200848aea5512349c07a000a92f313a74c98eb
luizmineiro <jluizmineiro@gmail.com>
2023-11-06 11:54:02 -0300

    loop for

---
+++ controle_de_fluxo/aula_4_for.dart
@@ -0,0 +1,11 @@
+void main() {
+  List<String> nomes = ["Theo", "João", "Alick", "Fernanda"];
+
+  // for (var i = 0; i < nomes.length; i++) {
+  //   print(nomes[i]);
+  // }
+
+  for (var alunos in nomes) {
+    print(alunos);
+  }
+}
