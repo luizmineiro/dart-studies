7fa180703ac2e79b1f808654abc9fec81417f492
luizmineiro <jluizmineiro@gmail.com>
2023-11-21 09:06:59 -0300

    tratamentos de erros

---
+++ tratamento_de_erros/aula_3_throw.dart
@@ -0,0 +1,21 @@
+void main() {
+  List<int> listaDeInteiros = [2, 10, 20, 8, 4, 80, 75, 59, 6, 54];
+  try {
+    for (var numerosInteiros in listaDeInteiros) {
+      if (numerosInteiros <= 10) {
+        // trhow => lançar
+        throw NumerosMenorOuIgaulA10Exepition(
+          'Numeros menores ou igual a 10 não são permitidos nessa aplicação',
+        );
+      }
+    }
+  } on NumerosMenorOuIgaulA10Exepition catch (error) {
+    print(error.message);
+  } catch (e) {}
+}
+
+class NumerosMenorOuIgaulA10Exepition implements Exception {
+  final message;
+
+  NumerosMenorOuIgaulA10Exepition(this.message);
+}
