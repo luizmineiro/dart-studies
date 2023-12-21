2af98e5844c7b888bac94db4bbeb9bd1f209fdb4
luizmineiro <jluizmineiro@gmail.com>
2023-12-21 08:43:42 -0300

    atualizando packages

---
+++ streams/aula_1_streams_introducao/bin/aula_1_streams_introducao.dart
@@ -0,0 +1,5 @@
+import 'package:aula_1_streams_introducao/aula_1_streams_introducao.dart' as aula_1_streams_introducao;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aula_1_streams_introducao.calculate()}!');
+}
---
+++ streams/aula_1_streams_introducao/test/aula_1_streams_introducao_test.dart
@@ -0,0 +1,8 @@
+import 'package:aula_1_streams_introducao/aula_1_streams_introducao.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
