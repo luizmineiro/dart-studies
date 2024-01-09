abd1417771ead59750ac91df39ceb603c8e879b3
luizmineiro <jluizmineiro@gmail.com>
2024-01-09 09:47:32 -0300

    generics

--- conceitos_finais/aula_3_generic_types/bin/aula_3_generic_types.dart
+++ conceitos_finais/aula_3_generic_types/bin/aula_3_generic_types.dart
@@ -1,3 +1,21 @@
 void main() {
   //* Tipo genericos
+
+  final Map<String, dynamic> data = {"nome": "João"};
+  print("Map<String, dynamic>: ${data.runtimeType}");
+
+  final listaDeNumeros = List<String>.generate(5, (index) => "${index + 1}");
+  print("List<String>: ${listaDeNumeros.runtimeType}");
+
+  final respostaDaApi = RespostaDaAPI<List<Map>>([
+    {"nome": "Luiz"},
+    {"nome": "Bia"},
+  ]);
+  print("TipoDaRespostaDaAPI - List<Map> ${respostaDaApi.resposta.runtimeType}");
+}
+
+class RespostaDaAPI<TipoDaRespostaDaAPI> {
+  final TipoDaRespostaDaAPI resposta;
+
+  RespostaDaAPI(this.resposta);
 }
