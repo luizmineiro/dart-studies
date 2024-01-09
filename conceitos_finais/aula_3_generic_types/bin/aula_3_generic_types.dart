ddded732d22ff84fed3161b121bb8c2efd5d2092
luizmineiro <jluizmineiro@gmail.com>
2024-01-08 21:38:34 -0300

    modulo de conceitos finais

---
+++ conceitos_finais/aula_3_generic_types/bin/aula_3_generic_types.dart
@@ -0,0 +1,3 @@
+void main() {
+  //* Tipo genericos
+}
---
+++ conceitos_finais/aula_3_generic_types/test/aula_3_generic_types_test.dart
@@ -0,0 +1,8 @@
+import 'package:aula_3_generic_types/aula_3_generic_types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
